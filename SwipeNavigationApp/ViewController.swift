c187539c11544a9892aa1e7c7e392e3e26bf83e2
gan gan <mynameisgan10@gmail.com>
2019-04-29 21:28:03 +0800

    Initial Commit

---
+++ SwipeNavigationApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwipeNavigationApp
+//
+//  Created by Gan on 29/4/19.
+//  Copyright © 2019 Gan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
