2563484cb006ede4f7cc46e18db47d17c349a7d2
gan gan <mynameisgan10@gmail.com>
2019-04-29 21:46:20 +0800

    swiping navigation works

---
+++ SwipeNavigationApp/SwipeController.swift
@@ -0,0 +1,36 @@
+//
+//  SwipeController.swift
+//  SwipeNavigationApp
+//
+//  Created by Gan on 29/4/19.
+//  Copyright © 2019 Gan. All rights reserved.
+//
+
+import UIKit
+
+class SwipingController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.backgroundColor = .white
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellid")
+        collectionView.isPagingEnabled = true
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellid", for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: view.frame.height
+        )
+    }
+}
